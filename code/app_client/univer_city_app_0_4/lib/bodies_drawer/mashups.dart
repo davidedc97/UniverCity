b83ad7b0876545744abbfbf2c8f06f39c148edc0
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2019-02-16 21:32:03 +0100

    inizio feedback

--- code/app_client/univer_city_app_0_4/lib/bodies_drawer/mashups.dart
+++ code/app_client/univer_city_app_0_4/lib/bodies_drawer/mashups.dart
@@ -3,7 +3,30 @@ import 'package:flutter/material.dart';
 class Mashups extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Text('Coming soon',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.brown[200],
+                )),
+            Icon(
+              Icons.insert_drive_file,
+              color: Colors.brown[200],
+              size: 150,
+            )
+          ],
+        )
+      ],
+    );
   }
 }
-
--- code/app_client/univer_city_app_0_4/lib/bodies_drawer/send_feedback.dart
+++ code/app_client/univer_city_app_0_4/lib/bodies_drawer/send_feedback.dart
@@ -6,7 +6,7 @@ class SendFeedback extends StatefulWidget {
 }
 
 class _SendFeedbackState extends State<SendFeedback> {
-  double _slider1 = 3;
+  double _slider1 = 1;
   int _radioVal;
   @override
   Widget build(BuildContext context) {
@@ -15,117 +15,48 @@ class _SendFeedbackState extends State<SendFeedback> {
         child: ListView(
           padding: EdgeInsets.all(16.0),
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Domanda 1',
-                    hintText: 'suggerimento',
-                  ),
-                ),
-                Divider(),
-                Text('Domanda 2'),
-                Slider(
-                    value: _slider1,
-                    min: 1.0,
-                    max: 5.0,
-                    divisions: 4,
-                    label: '${_slider1.round()}',
-                    onChanged: (double value) {
-                      setState(() {
-                        return _slider1 = value;
-                      });
-                    }),
-                Divider(),
-                TextField(
-                  maxLines: 4,
-                  decoration: InputDecoration(
-                    labelText: 'Domanda 3',
-                    hintText: 'suggerimento',
-                  ),
-                ),
-                Divider(),
-                Text('Domanda 4'),
-                Column(
+            Padding(
+                padding: EdgeInsets.only(top: 50),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
                   children: <Widget>[
-                    ListTile(
-                      leading: Radio<int>(
-                        value: 0,
-                        groupValue: this._radioVal,
-                        onChanged: (int value) {
-                          setState(() {
-                            this._radioVal = value;
-                          });
-                        },
-                      ),
-                      title: Text('si'),
-                      onTap: () {
-                        setState(() {
-                          this._radioVal = 0;
-                        });
-                      },
+                    Text("Che voto daresti all'app?"),
+                    Padding(
+                      padding: EdgeInsets.only(top: 32),
+                      child: Slider(
+                          value: _slider1,
+                          min: 1.0,
+                          max: 5.0,
+                          divisions: 4,
+                          label: '${_slider1.round()}',
+                          onChanged: (double value) {
+                            setState(() {
+                              return _slider1 = value;
+                            });
+                          }),
                     ),
-                    ListTile(
-                      leading: Radio<int>(
-                        value: 1,
-                        groupValue: this._radioVal,
-                        onChanged: (int value) {
-                          setState(() {
-                            this._radioVal = value;
-                          });
-                        },
+                    Divider(),
+                    TextField(
+                      maxLines: 6,
+                      decoration: InputDecoration(
+                        labelText:
+                            'Quali suggerimenti daresti per migliorarla?',
                       ),
-                      title: Text('piu si che no'),
-                      onTap: () {
-                        setState(() {
-                          this._radioVal = 1;
-                        });
-                      },
                     ),
-                    ListTile(
-                      leading: Radio<int>(
-                        value: 2,
-                        groupValue: this._radioVal,
-                        onChanged: (int value) {
-                          setState(() {
-                            this._radioVal = value;
-                          });
-                        },
-                      ),
-                      title: Text('piu no che si'),
-                      onTap: () {
-                        setState(() {
-                          this._radioVal = 2;
-                        });
-                      },
-                    ),
-                    ListTile(
-                      leading: Radio<int>(
-                        value: 3,
-                        groupValue: this._radioVal,
-                        onChanged: (int value) {
-                          setState(() {
-                            this._radioVal = value;
-                          });
-                        },
-                      ),
-                      title: Text('no'),
-                      onTap: () {
-                        setState(() {
-                          this._radioVal = 3;
-                        });
-                      },
-                    ),
-                    RaisedButton.icon(
-                        color: Colors.amber[200],
-                        icon: Icon(Icons.file_upload),
-                        label: Text('SEND'),
-                        onPressed: ()=>debugPrint('send')
+                    Divider(),
+                    Column(
+                      children: <Widget>[
+                        RaisedButton.icon(
+                            color: Colors.amber[200],
+                            icon: Icon(Icons.file_upload),
+                            label: Text('SEND'),
+                            onPressed: () => debugPrint('send')),
+                      ],
                     ),
                   ],
-                ),
-              ],
-            ),
+                ))
           ],
         ));
   }

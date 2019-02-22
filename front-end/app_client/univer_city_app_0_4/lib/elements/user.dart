721d8dcf8379a23ece3b0e4ba927929dd9b7717c
davidedc97 <davidedc1997@gmail.com>
2019-02-22 16:18:57 +0100

    Changes to be committed: 	modified:   front-end/app_client/univer_city_app_0_4/lib/elements/document_formatting.dart 	new file:   front-end/app_client/univer_city_app_0_4/lib/elements/user.dart 	modified:   front-end/app_client/univer_city_app_0_4/lib/http_handling/http_handler.dart

---
+++ front-end/app_client/univer_city_app_0_4/lib/elements/user.dart
@@ -0,0 +1,13 @@
+class User{
+
+  String _user;
+  String _name;
+  String _surname;
+  String _email;
+  String _password;
+  String _faculty;
+  String _University;
+
+  User(this._user, this._name, this._surname, this._email, this._password);
+
+}

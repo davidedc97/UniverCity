40223f0a5cdc45add5625fa56535962473a71e4e
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2019-02-22 16:45:48 +0100

    bd init done

--- front-end/app_client/univer_city_app_0_4/lib/db/repository.dart
+++ front-end/app_client/univer_city_app_0_4/lib/db/repository.dart
@@ -16,9 +16,19 @@ class DocumentProvider{
       path,
       version: 1,
       onCreate: (Database newDB, int version){
-        
+        newDB.execute('''
+        CREATE TABLE Documenti(
+          title TEXT NOT NULL,
+          owner TEXT NOT NULL,
+          documentuuid TEXT PRIMARY KEY,
+        )
+        ''');
       }
     );
+  }
+
+  fetchItem() async{
+
   }
 
 }

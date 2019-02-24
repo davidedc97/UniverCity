8a3cd42e574515e0a3cd16438b528d3964292eaf
IsfetGIT <43957949+IsfetGIT@users.noreply.github.com>
2019-02-24 17:52:23 +0100

    login v1.0

--- back-end/loginv0.1/login.js
+++ back-end/loginv0.1/login.js
@@ -1,52 +1,58 @@
 
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
-var bodyP = require("body-parser");
-app.use(bodyP.urlencoded({estended: false}));
+const bodyP = require("body-parser");
+app.use(bodyP.urlencoded());
 
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "isfet",
-    password: ""
+    host: "127.0.0.1",
+    user: "root",
+    password: "dinamik",
+    port: 3306,
+    database: "userdb"
 })
 
 connection.connect(function(err){
     if(err) throw err;
-    console.log("DB logged");
+    console.log(err);
 })
 
-function DbManager(){
-    connection.query("CREATE DATABASE UserDB", function(err, result){
-        if(err) console.log(err);
-    })
-
-    connection.query("CREATE TABLE User (Username VARCHAR(255), name VARCHAR(255), surname VARCHAR(255), email VARCHAR(255), pass VARCHAR(255))", function(err, result){
-        if(err) console.log(err);
-    })
-}
-
-function DbManager_table(user, pass){
-
-    connection.query("INSERT INTO User (name, surname) VALUES (user, pass)", function(err, result){
-        if(err) console.log(err);
-    })
-
-    connection.query("SELECT * FROM User", function(err, result){
-        if(err) console.log(err);
+function DbManager_addUser(ur, pr){
+    var username = ur;
+    var pass = pr;
+    console.log("dati letti" + " " + username + " " + pass);
+    var control = 0; 
+    connection.query("INSERT INTO user (username,pass) VALUES (username, pass)", function(err, result){
+        if(err) throw err;
+        console.log(err);
+        control = 1;
         console.log(result);
     })
+    return control;
 }
 
-app.route("/")
+function DbManager_searchUser(ul, pl){
+    var username = ul;
+    var pass = pl;
+    connection.query("SELECT username FROM user", function(err, result){
+        if(err) throw err;
+        console.log(err);
+        control = 1;
+    })
+    return control;
+}
 
-    .get(function(req,res){
-        res.sendFile(__dirname + "/simple_we_page.html");
+app.get("/", function(req,res){
+        res.sendFile(__dirname + "/index.html");
+        //res.sendStatus(200);
     })
 
-    .post(function(req, res){
+app.post("/", function(req, res){
+        var control;
+        console.log("##################");
         console.log("Richiesta ricevuta");
         console.log("##################");
     
@@ -57,7 +63,8 @@ app.route("/")
             console.log("Invio dati: ");
             console.log("User:" + ur);
             console.log("Pass: " + pr);
-            //DbManager(ur,pr);
+            control = DbManager_addUser(ur,pr);
+            console.log("control = " + control);
         }
 
         if (req.body.ul){
@@ -67,14 +74,18 @@ app.route("/")
             console.log("Invio dati: ");
             console.log("User:" + ul);
             console.log("Pass: " + pl);
-            //DbManager(ul,pl);
+            control = DbManager_searchUser(ul,pl);
+            if (control == 1) res.sendStatus(400);
+            else res.sendStatus(200);
+            control = 0;
         }
     })
 
-    .get(function(req, res){
+app.get("/",function(req, res){
         console.log("terminated");
     })
 
-var server = app.listen(1234, "127.0.0.1", function(){
-    console.log("Started");
+
+var server = app.listen(8080, "127.0.0.1", function(){
+    console.log("Server started");
 })

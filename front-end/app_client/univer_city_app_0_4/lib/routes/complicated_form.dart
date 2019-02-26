b017273a60e6fdca5a0b9fd72a98f6b9846730a4
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2019-02-26 12:41:05 +0100

    sistemati piccoli errori future builder login e complicated form

--- front-end/app_client/univer_city_app_0_4/lib/routes/complicated_form.dart
+++ front-end/app_client/univer_city_app_0_4/lib/routes/complicated_form.dart
@@ -8,7 +8,6 @@ class CompFormScaffold extends StatefulWidget {
 }
 
 class _CompFormScaffoldState extends State<CompFormScaffold> {
-
   String _nm, _em, _pw, _fa, _cg, _us, _un;
 
   @override
@@ -27,58 +26,80 @@ class _CompFormScaffoldState extends State<CompFormScaffold> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-              Text('COMPLICATED FORM',style: TextStyle(fontSize: 18),),
-              SizedBox(height: 24,),
+              Text(
+                'COMPLICATED FORM',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(
+                height: 24,
+              ),
               TextField(
-                  onChanged: (value){_us=value;},
+                  onChanged: (value) {
+                    _us = value;
+                  },
                   textAlign: TextAlign.center,
                   decoration: InputDecoration(
                     hintText: 'User',
                   )),
               TextField(
-                  onChanged: (value){_nm=value;},
+                  onChanged: (value) {
+                    _nm = value;
+                  },
                   textAlign: TextAlign.center,
                   decoration: InputDecoration(
                     hintText: 'Nome',
                   )),
               TextField(
-                  onChanged: (value){_cg=value;},
+                  onChanged: (value) {
+                    _cg = value;
+                  },
                   textAlign: TextAlign.center,
                   decoration: InputDecoration(
                     hintText: 'Cognome',
                   )),
               TextField(
-                  onChanged: (value){_em=value;},
+                  onChanged: (value) {
+                    _em = value;
+                  },
                   textAlign: TextAlign.center,
                   decoration: InputDecoration(
                     hintText: 'Email',
                   )),
               TextField(
                   obscureText: true,
-                  onChanged: (value){_pw=value;},
+                  onChanged: (value) {
+                    _pw = value;
+                  },
                   textAlign: TextAlign.center,
                   decoration: InputDecoration(
                     hintText: 'Password',
                   )),
               TextField(
-                  onChanged: (value){_fa=value;},
+                  onChanged: (value) {
+                    _fa = value;
+                  },
                   textAlign: TextAlign.center,
                   decoration: InputDecoration(
                     hintText: 'Facoltà',
                   )),
               TextField(
-                  onChanged: (value){_un=value;},
+                  onChanged: (value) {
+                    _un = value;
+                  },
                   textAlign: TextAlign.center,
                   decoration: InputDecoration(
                     border: InputBorder.none,
                     hintText: 'Università',
                   )),
-              SizedBox(height: 42,),
+              SizedBox(
+                height: 42,
+              ),
               //################################################################ Get Started Button
               BtnLogin(
                 color: Colors.redAccent[700],
                 title: 'GET STARTED',
-                onPressed: ()=>compForm(context, _us??'', _nm??'', _cg??'', _em??'', _pw??'', _fa??'', _un??''),
+                onPressed: () => compForm(context, _us ?? '', _nm ?? '',
+                    _cg ?? '', _em ?? '', _pw ?? '', _fa ?? '', _un ?? ''),
               ),
               //################################################## LOGIN if already have an account
               Row(
@@ -102,33 +123,62 @@ class _CompFormScaffoldState extends State<CompFormScaffold> {
   }
 }
 
-compForm(BuildContext context, String id, String nm, String cg, String em, String pw, String fa, String un){
-  if(id=='' || nm=='' || cg=='' || em=='' || pw=='' || fa=='' || un==''){
+compForm(BuildContext context, String id, String nm, String cg, String em,
+    String pw, String fa, String un) {
+  if (id == '' ||
+      nm == '' ||
+      cg == '' ||
+      em == '' ||
+      pw == '' ||
+      fa == '' ||
+      un == '') {
     return showDialog(
         context: context,
-        builder: (context){
+        builder: (context) {
           return AlertDialog(
             title: Text('Ops!'),
             content: Text('something in the form is not valid'),
             actions: <Widget>[
-              FlatButton(onPressed: (){Navigator.pop(context);}, child: Text('Close'))
+              FlatButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Close'))
             ],
           );
-        }
-    );
-  }else{
+        });
+  } else {
     //TODO da testare
-    debugPrint('email: $id, Nome: $nm, Cognome: $cg, Email: $em, Pass: $pw, Facolta $fa ');
+    debugPrint(
+        'email: $id, Nome: $nm, Cognome: $cg, Email: $em, Pass: $pw, Facolta $fa ');
     Future res = HttpHandler.sendFormRegistration(id, nm, cg, em, pw, fa, un);
     FutureBuilder<bool>(
       future: res,
-      builder: (context, snapshot){
-        if(snapshot.hasError) print(snapshot.data);
+      builder: (context, snapshot) {
+        if (snapshot.hasError) print(snapshot.data);
         return snapshot.hasData
-            ? Navigator.pushNamedAndRemoveUntil(context, '/', (Route<dynamic> route) => false)
-            : Center(child: CircularProgressIndicator(),);
+            ? (snapshot.data)
+                ? Navigator.pushNamedAndRemoveUntil(
+                    context, '/', (Route<dynamic> route) => false)
+                : showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Ops!'),
+                        content: Text('something went wrong'),
+                        actions: <Widget>[
+                          FlatButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text('Close'))
+                        ],
+                      );
+                    })
+            : Center(
+                child: CircularProgressIndicator(),
+              );
       },
     );
-
   }
 }
--- front-end/app_client/univer_city_app_0_4/lib/routes/login_form.dart
+++ front-end/app_client/univer_city_app_0_4/lib/routes/login_form.dart
@@ -5,7 +5,6 @@ import 'package:univer_city_app_0_4/http_handling/http_handler.dart';
 class LoginFormScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     String _usem, _pw;
 
     return Scaffold(
@@ -22,31 +21,42 @@ class LoginFormScaffold extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-              Text('LOGIN',style: TextStyle(fontSize: 18),),
-              SizedBox(height: 24,),
+              Text(
+                'LOGIN',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(
+                height: 24,
+              ),
               TextField(
-                onChanged: (value){
-                  //bloc.email.add(value);
-                  _usem=value;
-                },
+                  onChanged: (value) {
+                    //bloc.email.add(value);
+                    _usem = value;
+                  },
                   textAlign: TextAlign.center,
                   decoration: InputDecoration(
                     hintText: 'User o Email',
                   )),
               TextField(
                   obscureText: true,
-                  onChanged: (value){_pw=value;},
+                  onChanged: (value) {
+                    _pw = value;
+                  },
                   textAlign: TextAlign.center,
                   decoration: InputDecoration(
                     border: InputBorder.none,
                     hintText: 'Password',
                   )),
-              SizedBox(height: 42,),
+              SizedBox(
+                height: 42,
+              ),
               //################################################################ LOGIN BUTTON
               BtnLogin(
                 color: Colors.redAccent[700],
                 title: 'LOGIN',
-                onPressed: (){login(context, _usem??'', _pw??'');},
+                onPressed: () {
+                  login(context, _usem ?? '', _pw ?? '');
+                },
               ),
               //################################################## LOGIN if already have an account
               Row(
@@ -56,8 +66,8 @@ class LoginFormScaffold extends StatelessWidget {
                   Text('Forgot password?'),
                   FlatButton(
 
-                    ///Login per ora ti rimanda nell'home page
-                    ///Poi ci saranno da gestire piu cose
+                      ///Login per ora ti rimanda nell'home page
+                      ///Poi ci saranno da gestire piu cose
                       onPressed: () {
                         debugPrint('Recover');
                       },
@@ -71,39 +81,59 @@ class LoginFormScaffold extends StatelessWidget {
       ]),
     );
   }
-
 }
+
 ///
 /// controllo form
 ///
-login(BuildContext context, String id, String pw ){
-  if(id == '' || pw == ''){
+login(BuildContext context, String id, String pw) {
+  if (id == '' || pw == '') {
     return showDialog(
         context: context,
-        builder: (context){
+        builder: (context) {
           return AlertDialog(
             title: Text('Ops!'),
             content: Text('something in the form is not valid'),
             actions: <Widget>[
-              FlatButton(onPressed: (){Navigator.pop(context);}, child: Text('Close'))
+              FlatButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Close'))
             ],
           );
-        }
-    );
-  }else{
+        });
+  } else {
     //TODO da testare
     debugPrint('email: $id, pass: $pw ');
     Future res = HttpHandler.validateLogin(id, pw);
     FutureBuilder<bool>(
       future: res,
-      builder: (context, snapshot){
-        if(snapshot.hasError) print(snapshot.data);
+      builder: (context, snapshot) {
+        if (snapshot.hasError) print(snapshot.data);
         return snapshot.hasData
-            ? Navigator.pushNamedAndRemoveUntil(context, '/', (Route<dynamic> route) => false)
-            : Center(child: CircularProgressIndicator(),);
+            ? (snapshot.data)
+                ? Navigator.pushNamedAndRemoveUntil(
+                    context, '/', (Route<dynamic> route) => false)
+                : showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Ops!'),
+                        content: Text('incorrect user or password'),
+                        actions: <Widget>[
+                          FlatButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text('Close'))
+                        ],
+                      );
+                    })
+            : Center(
+                child: CircularProgressIndicator(),
+              );
       },
     );
-
-
   }
 }

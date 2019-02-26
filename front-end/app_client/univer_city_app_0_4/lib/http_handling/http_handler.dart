882d3e76a8d74359d66e1b8a53228145afb7a31b
davidedc97 <davidedc1997@gmail.com>
2019-02-26 17:20:41 +0100

    Changes to be committed: 	modified:   front-end/app_client/univer_city_app_0_4/lib/http_handling/http_handler.dart

--- front-end/app_client/univer_city_app_0_4/lib/http_handling/http_handler.dart
+++ front-end/app_client/univer_city_app_0_4/lib/http_handling/http_handler.dart
@@ -74,7 +74,7 @@ class HttpHandler {
     }
   }
 
-
+  //commento a caso per pushare
   static Future getDocumentById(docId) async{
     final response =
       await http.get(_URL + _DOCUMENT_SERVER + "/" + docId);

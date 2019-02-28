bf12b5e9cda6783b73aa336a3d6566141dfcfb74
IsfetGIT <43957949+IsfetGIT@users.noreply.github.com>
2019-03-01 00:14:39 +0100

    wrapping cli req

--- back-end/cli_call_wrapp/cognito_Wrap.js
+++ back-end/cli_call_wrapp/cognito_Wrap.js
@@ -1,10 +1,33 @@
+var express = require('express');
+const bodyP = require("body-parser");
 var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
 var AWS = require('aws-sdk');
 var request = require('request');
 var jwkToPem = require('jwk-to-pem');
 var jwt = require('jsonwebtoken');
+var crypto = require('crypto');
 global.fetch = require('node-fetch');
+
+var username = null;
+var name = null;
+var surname = null;
+var email = null;
+var pass = null;
+var faculty = null;
+var university = null;
+var token = null;
+var error = null;
+
+/*
+** ########################
+** # SERVER CONFIGURATION #
+** ########################
+*/
+
+var app = express();
+
+app.use(bodyP.urlencoded());
 
 const poolData = {
     UserPoolId: "eu-west-2_TBU678aQA",
@@ -15,20 +38,67 @@ const pool_region= "eu-west-2";
 
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
+var server = app.listen(8080, "127.0.0.1", function(){
+    console.log("Server started");
+})
+
 /*
-**  ########################
-**  # REGISTRATION PROCESS #
-**  ########################
+** ###################
+** # CLIENT WRAPPING #
+** ###################
+**
+*/
+
+app.post("/user", function(req, res){
+    username = req.body.username;
+    name = req.body.name;
+    surname = req.body.surname;
+    email = req.body.email;
+    pass = req.body.pass;
+    faculty = req.body.faculty;
+    university = req.body.university;
+    
+    var control = signUp();
+
+    if (control == null) res.sendStatus(400).statusMessage("User already exist");
+    else {
+        res.sendStatus(200);
+    }
+})
+
+app.post("/user", function(req, res){
+    username = req.body.username;
+    pass = req.body.pass;
+
+    var control = SignIn();
+
+    if (control == 0) res.sendStatus(200);
+    else {
+        res.sendStatus(400);
+    }
+})
+
+// getOtherUserById and getMyUserById MISSING
+
+/*
+** ########################
+** # REGISTRATION PROCESS #
+** ########################
 */
 
 function SignUp(){
+
+    var pwd = username + Date();
+    var id = crypto.createHash('sha256').update().digest;
+
     var attList = [];
-    attList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: "name", Value: ""}));
-    attList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: "surname", Value: ""}));
-    attList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: "email", Value: ""}));
-    attList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: "faculty", Value: ""}));
-    attList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: "university", Value: ""}));
-    attList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: "preferrred_username", Value: ""}));
+    attList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: "id", Value: id}));
+    attList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: "name", Value: name}));
+    attList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: "surname", Value: surname}));
+    attList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: "email", Value: email}));
+    attList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: "faculty", Value: faculty}));
+    attList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: "university", Value: university}));
+    attList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: "preferrred_username", Value: username}));
 
     userPool.signUp("<username>", "<pass>", attList, null, function(err, res){
         if(err){
@@ -38,37 +108,33 @@ function SignUp(){
         return cognitoUser.getUsername();
     })
 }
-
-// manca wrapping chiamate da client
-
 /*
-**  #################
-**  # LOGIN PROCESS #
-**  #################
+** #################
+** # LOGIN PROCESS #
+** #################
 */
 
 function SignIn(){
     var autenticationValue = new AmazonCognitoIdentity.AuthenticationDetails({
-        Username: "<username/emai>",
-        Password: "<pass>"
+        Username: username,
+        Password: pass
     })
 
     var userData = {
-        Username: "<username/emai>",
+        Username: username,
         Pool: userPool
     }
 
     var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
     cognitoUser.authenticateUser(autenticationValue, {
         onSuccess: function(res) {
-            console.log(res.getAccessToken().getJwtToken());
-            console.log(res.getIdToken().getJwtToken());
-            console.log(res.getRefreshToken().getJwtToken());
+            token = res.getAccessToken();
         },
         onFailure: function(err) {
-            console.log(err);
+            error = err;
         },
     });
-}
 
-// manca wrapping chiamate da client
+    if (token != null && error == null) return 0;
+    else if (token == null && error != null) return 1;
+}

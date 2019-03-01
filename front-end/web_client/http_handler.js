c79f86e21b8e4fa1e66154c541f2f518ce8ee5e6
Marco170595 <46960620+Marco170595@users.noreply.github.com>
2019-03-01 18:47:22 +0100

    added some changes

--- front-end/web_client/http_handler.js
+++ front-end/web_client/http_handler.js
@@ -1,5 +1,4 @@
-//import ;  decomment when others put the correct link
-
+//import ;  importa quando gli altri hanno finito di modificare il github
 static function sendFormRegistration(user, name, surname, email, pw, faculty, university){
     var httpRequest = new XMLHttpRequest();
     var body = {"username": user, "name": name, "surname": surname, "email": email, "pass": pw, "faculty": faculty, "university":university};
@@ -82,6 +81,8 @@ static function getOtherUserById(userId){
 /*  ##### GESTIONE DOCUMENTI #####*/ 
 
 static function uploadDocument(title, type,  path){
+    var httpRequest = new XMLHttpRequest();
+    //var request = ;
     var uri = decodeURI("http://www.porcaccioiltuodio.mam" + "document"); //insert from import
     //TODO
 }
@@ -90,13 +91,75 @@ static function getDocumentById(docId){
     var httpRequest = new XMLHttpRequest();
 
     httpRequest.onreadystatechange = function(){
-        //if(httpRequest.readyState == 4)
-            //return JSON.stringify(docId);
+        if(httpRequest.readyState == 4){
+            return JSON.stringify(docId);
+        }
     };
 
     httpRequest.open("GET", "http://www.porcaccioiltuodio.mam", true );
     httpRequest.send();
 
     if(httpRequest.status == 200)
-        return JSON.stringify(docId);
+        return 1;
+}
+
+static function searchDocuments(query){
+    var httpRequest = new XMLHttpRequest();
+
+    httpRequest.onreadystatechange = function(){
+        if(httpRequest.readyState == 4){
+            var doc = document.getElementById("query");
+            return doc;
+        }
+    };
+
+    httpRequest.open("GET", "http://www.porcaccioiltuodio.mam", true);
+    httpRequest.send();
+
+    if(httpRequest.status == 200)
+        return 1;
+}
+
+//*******/TODO: static function uploadDocument(docId){} ********
+
+static function mashup(pageIds){
+    var httpRequest = new XMLHttpRequest();
+
+    httpRequest.onreadystatechange = function(){
+        if(httpRequest.readyState == 4){
+            var mashIdPages = document.getElementById(pageIds);
+            return mashIdPages;
+        }
+    }
+
+    httpRequest.open("POST", "http://www.porcaccioiltuodio.mam", true);
+    httpRequest.send(mashIdPages);
+
+    if(httpRequest.status == 200)
+        return 1;
+}
+
+                                /* ###### LIKES HANDLING  #########*/
+
+static function addLike(user, docId){
+    var httpRequest = new XMLHttpRequest();
+    var body = {"user": user, "doc_id": docId};
+
+    httpRequest.onreadystatechange = function(){
+        //TODO
+    }
+
+    httpRequest.open("POST", "http://www.porcaccioiltuodio.mam", true);
+    httpRequest.send(//TODO);
+}
+
+static function retrieveLike(docId){
+    var httpRequest = new XMLHttpRequest();
+
+    httpRequest.onreadystatechange = function(){
+        //TODO
+    }
+
+    httpRequest.open("GET", "http://www.porcaccioiltuodio.mam", true);
+    httpRequest.send();
 }

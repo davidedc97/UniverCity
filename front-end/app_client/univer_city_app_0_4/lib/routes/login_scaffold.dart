105172368fdbd329621ac92112112c4896696639
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2019-03-05 18:02:15 +0100

    sfo

--- front-end/app_client/univer_city_app_0_4/lib/routes/login_scaffold.dart
+++ front-end/app_client/univer_city_app_0_4/lib/routes/login_scaffold.dart
@@ -23,7 +23,7 @@ class LoginScaffold extends StatelessWidget {
           flex: 2,
           child: Container(
             decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/img/sfondo_login.png'), fit: BoxFit.fitWidth)
+              image: DecorationImage(image: AssetImage('assets/img/sfondo_login2.png'), fit: BoxFit.fitWidth)
             ),
             //color: Theme.of(context).scaffoldBackgroundColor,
           )),

5738750c583397e255b8b74deb1361fc38c3bbcb
Lucian <medrihanlucian@gmail.com>
2019-03-07 16:39:14 +0100

    error page

--- front-end/web_client/paginaRegistrazione/checkPassword.js
+++ front-end/web_client/paginaRegistrazione/checkPassword.js
@@ -1,8 +1,8 @@
-function CheckPassword(inputtxt) { 
-
+function CheckPassword() { 
+    pw=document.getElementById("psw").value;
     var decimal =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
 
-    if(inputtxt.value.match(decimal)) { 
+    if(pw.match(decimal)) { 
         alert('Correct, try another...')
         return true;
     }

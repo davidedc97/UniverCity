a9a50a892bbf2eb30b595af2337d21ad2fe2235b
Marco170595 <mcollalts23@gmail.com>
2019-03-07 18:40:27 +0100

    fixing login and password

--- front-end/web_client/paginaRegistrazione/checkPassword.js
+++ front-end/web_client/paginaRegistrazione/checkPassword.js
@@ -1,13 +1,18 @@
-function CheckPassword(inputtxt) { 
+function checkPassword() { 
 
-    var decimal =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+ //  var decimal =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8+}$/;
+    var decimal = /(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&])(?=.*[A-Z]).{8,}/;
+    var pw = document.registra.pass.value;
 
-    if(inputtxt.value.match(decimal)) { 
-        alert('Correct, try another...')
-        return true;
-    }
-    else{ 
-        alert('Wrong...!')
+   /* if(pw.length < 8){
+        alert("Password must be longer than 6 characters");
+        return false;
+    }*/
+
+    if( !document.registra.pass.value.match(decimal)) { 
+        //alert('Correct, try another...');
+        alert("Password must contain at least a special character, a number, a capital letter and must be longer than 8 characters");
         return false;
     }
+    return true;
 } 

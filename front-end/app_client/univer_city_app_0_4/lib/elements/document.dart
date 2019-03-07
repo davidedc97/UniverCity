ae2232b12fe0bdffaa997e9db2c10fdf14ac174e
davidedc97 <davidedc1997@gmail.com>
2019-03-07 18:25:05 +0100

    Changes to be committed: 	modified:   front-end/app_client/univer_city_app_0_4/lib/elements/doc_list.dart 	modified:   front-end/app_client/univer_city_app_0_4/lib/elements/document.dart

--- front-end/app_client/univer_city_app_0_4/lib/elements/document.dart
+++ front-end/app_client/univer_city_app_0_4/lib/elements/document.dart
@@ -1,34 +1,34 @@
 class Document{
 
   String _title;
-  String _owner;
+  String _creator;
   String _uuid;
   String _type; // it can be "M" (mashup) or "O" (original)
 
 
-  Document(this._title, this._owner, this._uuid, this._type);
+  Document(this._title, this._creator, this._uuid, this._type);
 
   String get title => _title;
-  String get owner => _owner;
+  String get creator => _creator;
   String get uuid => _uuid;
   String get type => _type;
 
   Document.fromJson(Map<String, dynamic> json) {
     //funzione-costruttore da usare in http_handler dentro la search_document/get_document
     this._title = json["title"];
-    this._owner = json["creator"];
+    this._creator = json["creator"];
     this._uuid = json["id"];
     this._type = json["type"];
   }
 
   @override String toString(){
-    return "titolo: " + this.title + "; creatore: " + this.owner + "; id: " + this.uuid + ", tipo: " + this.type + "\n";
+    return "titolo: " + this.title + "; creatore: " + this.creator + "; id: " + this.uuid + ", tipo: " + this.type + "\n";
   }
 
   Map<String, dynamic> toMap(){
     return <String, dynamic> {
       'title': _title,
-      'owner': _owner,
+      'owner': _creator,
       'uuid': _uuid,
     };
   }

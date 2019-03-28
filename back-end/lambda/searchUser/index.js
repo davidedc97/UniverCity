3aaeb86942d766c943622a672e039d46fbfc2f23
IsfetGIT <43957949+IsfetGIT@users.noreply.github.com>
2019-03-28 19:26:02 +0100

    added searchUser lambda func

---
+++ back-end/lambda/searchUser/index.js
@@ -0,0 +1,65 @@
+
+const AWS = require('aws-sdk');
+global.fetch = require('node-fetch');
+
+var Pool = require('pg').Pool;
+
+const psql = new Pool ({
+    host: 'metadata.czzhwg1jheui.eu-west-1.rds.amazonaws.com',
+    user: 'univercity',
+    password: '',
+    database: 'metadata',
+    port: 5432
+})
+
+function searchUser(username){
+    return new Promise((resolve, reject) => {
+        psql.query("SELECT username FROM utilitator where username LIKE '%$1%'", [username], {
+            onSucces: function(res){
+                resolve(res.rows);
+            },
+            onFailure: function(err){
+                resolve("err");
+            },
+        });
+    });
+}
+
+exports.handler = async (event, context, callback) => {
+    var body = JSON.parse(event.body);
+    
+    var username = body.username;
+
+    var response = {
+        "statusCode": 200,
+        "isBase64Encoded": false,
+        "body": {},
+    }
+    
+    try{
+        var result = await searchUser(username).then((result) => {
+            return result;
+        });
+        
+        var statusCode = 200;
+        var body = result;
+        if(result == "err"){
+            statusCode = 400;
+            body = "user not found";
+        }
+        
+        response.statusCode = statusCode;
+        response.body = body;
+        
+        callback(null,response);
+    }
+    catch(e){
+        callback(e,{
+            "isBase64Encoded": false,
+            "headers": {},
+            "body": "err",
+            "statusCode": 501
+        });
+    }
+};
+

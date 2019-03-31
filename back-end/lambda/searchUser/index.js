9a2b1d1bef20451c6e968b40ea35a8ab61d708ff
IsfetGIT <43957949+IsfetGIT@users.noreply.github.com>
2019-04-01 01:18:32 +0200

    fixed metadata server

--- back-end/lambda/searchUser/index.js
+++ back-end/lambda/searchUser/index.js
@@ -7,59 +7,78 @@ var Pool = require('pg').Pool;
 const psql = new Pool ({
     host: 'metadata.czzhwg1jheui.eu-west-1.rds.amazonaws.com',
     user: 'univercity',
-    password: '',
+    password: 'googleworkshop',
     database: 'metadata',
     port: 5432
 })
 
-function searchUser(username){
-    return new Promise((resolve, reject) => {
-        psql.query("SELECT username FROM utilitator where username LIKE '%$1%'", [username], {
-            onSucces: function(res){
-                resolve(res.rows);
-            },
-            onFailure: function(err){
-                resolve("err");
-            },
-        });
-    });
+/*var test = [];
+var control;
+
+async function searchUser(username){
+
+    var result;
+
+    await psql.query("SELECT username from utilitator where username = $1", [username], function(err, res){
+        if (err) control = "err";
+        else {
+            result = res.rows;
+            var i;
+            for (i = 0; i<result.length; i++){
+                test.push(result[i].username);
+            }
+        } 
+    })
 }
 
-exports.handler = async (event, context, callback) => {
-    var body = JSON.parse(event.body);
-    
-    var username = body.username;
+exports.handler = async (event) => {
+    //const body = JSON.parse(event.body);
+  
+    const username = event.username;
+    await searchUser(username);
 
-    var response = {
-        "statusCode": 200,
+    if (control == "err") {
+      return {
+        "statusCode": 404,
         "isBase64Encoded": false,
-        "body": {},
+        "body": JSON.stringify({message: 'User not found'}),
+      }
     }
-    
-    try{
-        var result = await searchUser(username).then((result) => {
-            return result;
-        });
-        
-        var statusCode = 200;
-        var body = result;
-        if(result == "err"){
-            statusCode = 400;
-            body = "user not found";
-        }
-        
-        response.statusCode = statusCode;
-        response.body = body;
-        
-        callback(null,response);
+    return {
+      "statusCode": 200,
+      "isBase64Encoded": false,
+      "body": JSON.stringify(test)
     }
-    catch(e){
-        callback(e,{
-            "isBase64Encoded": false,
-            "headers": {},
-            "body": "err",
-            "statusCode": 501
-        });
-    }
-};
+  
+  };
+*/
 
+async function searchUser(username){
+    return await psql.query('SELECT username FROM utilitator where username LIKE $1', [`%${username}%`])
+  };
+
+  exports.handler = async (event) => {
+    //const body = JSON.parse(event.body);
+    var ret = [];
+    const username = event.username;
+  
+    const result = await searchUser(username)
+  
+    var i;
+    for (i = 0; i< result.rows.length; i++){
+        ret.push(result.rows[i].username);
+    }
+    if (result == "err") {
+      return {
+        "statusCode": 404,
+        "isBase64Encoded": false,
+        "body": JSON.stringify({message: 'User not found'}),
+      }
+    }
+    return {
+      "statusCode": 200,
+      "isBase64Encoded": false,
+      "body": JSON.stringify(ret),
+    }
+  
+};

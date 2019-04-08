4a83acaaa45fa201ffbabcdcc452cb37cdb5ce39
Tiziano Colagrossi <tiziano.colagrossi@gmail.com>
2019-04-08 14:29:10 +0200

    merge

--- front-end/app_client/univer_city_app_1_1/lib/bloc/preferiti_bloc.dart
+++ front-end/app_client/univer_city_app_1_1/lib/bloc/preferiti_bloc.dart
@@ -1,33 +1,83 @@
-import 'package:rxdart/rxdart.dart';
-import 'package:univer_city_app_1_1/elements/session_user.dart';
 import 'package:univer_city_app_1_1/http_handling/http_handler.dart';
+import 'package:univer_city_app_1_1/elements/session_user.dart';
+import 'package:rxdart/rxdart.dart';
 
 class PreferitiBloc {
 
-  final BehaviorSubject<List<dynamic>> _preferiti = BehaviorSubject<List<dynamic>>();
-  final BehaviorSubject<int> _num = BehaviorSubject<int>();
+  ///
+  /// Lista che contiene le entry visualizzate nella pagina drawer
+  /// dei preferiti
+  ///
+  static List<String> _pref = [];
 
-  Observable<List<dynamic>> get preferiti => _preferiti.stream;
-  Function(List<dynamic>) get onPreferitiChanged => _preferiti.sink.add;
+  ///
+  /// Stream per la lista in app
+  ///
+  final BehaviorSubject<List<String>> _preferiti =
+  BehaviorSubject<List<String>>(seedValue: []);
 
-  fetchData()async{
-    HttpHandler.getUserFavourites(SessionUser().user).then((List<dynamic> list){
-      if(list != null){
-        if(list.isNotEmpty){
-          if(list[0]=='-1'||list[0]=='-2'){
-            _preferiti.addError(list[0]);
+  ///
+  /// questo metodo è invocato durante l'inizializzazione del [BlocProvider]
+  /// legge le entry salvate e le reiserisce all'interno della lista [_preferiti]
+  ///
+  Future<List<dynamic>> init() async {
+    return HttpHandler.getUserFavourites(SessionUser.user)
+        .then((val){
+          _pref = [];
+          for(dynamic e in val){
+            print('add e in fav');
+            _pref.add(e.trim());
           }
-          else{
-            _num.sink.add(list.length);
-            onPreferitiChanged(list);
-          }
+          print('init pref ${_pref}');
+          onPreferitiChanged(_pref);
+          print('Stream notified');
+          _save();
         }
-      }
-    });
+    );
+  }
+
+  ///
+  /// getter agli stream e sink
+  ///
+  Observable<List<String>> get preferiti => _preferiti.stream;
+  List<String> get preferitiValue => _preferiti.value;
+  Function get onPreferitiChanged => _preferiti.sink.add;
+
+  ///
+  /// Funzione per aggiungere un elemento in [_pref]
+  ///
+  addInFavourite(dynamic e) {
+    print('add fav');
+    HttpHandler.addUserFavourite(SessionUser.user, e);
+    _pref.add(e);
+    onPreferitiChanged(_pref);
+    _save();
+    print('end add fav');
+  }
+
+  ///
+  /// Funzione per rimuovere un elemento in [_pref]
+  ///
+  removeInFavourite(dynamic e) {
+    print('rem fav');
+    HttpHandler.removeUserFavourite(SessionUser.user, e);
+    _pref.remove(e);
+    onPreferitiChanged(_pref);
+    _save();
+    print('end rem fav');
+  }
+
+  ///
+  /// Funzione per salvare [_pref] in una classe statica usata
+  /// per la sessione locale
+  ///
+  _save() {
+    print('save');
+    SessionUser.setPreferiti = _pref;
+    print('end save');
   }
 
   dispose(){
     _preferiti.close();
-    _num.close();
   }
 }
